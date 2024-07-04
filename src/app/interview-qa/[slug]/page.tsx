f898dc1fbe5dee5beb8b121f70de637d08d326e1
Arpit <arpits4121@gmail.com>
2024-07-04 18:13:20 +0530

    SEO Changes

--- src/app/interview-qa/[slug]/page.tsx
+++ src/app/interview-qa/[slug]/page.tsx
@@ -1,15 +1,30 @@
+import type { Metadata, ResolvingMetadata } from "next";
 import React, { Fragment } from "react";
 import InterviewQAComponent from "@/components/InterviewQA/InterviewQAComponent";
-import { Metadata } from "next";
 
-export const metadata: Metadata = {
-   title: `InterviewQA |  ItsIndianGuy`,
-   description:
-      "Explore interview questions in various programming languages such as JavaScript, Python, C++ and more. Enhance your preparation with comprehensive language-specific content.",
-   alternates: {
-      canonical: "https://itsindianguy.in/interview-qa",
-   },
+type Props = {
+   params: { slug: string };
+   searchParams: { [key: string]: string | string[] | undefined };
 };
+
+export async function generateMetadata(
+   { params }: Props,
+   parent: ResolvingMetadata
+): Promise<Metadata> {
+   // read route params
+   const slug = params.slug;
+
+   return {
+      title: `${
+         slug.charAt(0).toUpperCase() + slug.slice(1)
+      } InterviewQA |  ItsIndianGuy`,
+      description:
+         "Explore interview questions in various programming languages such as JavaScript, Python, C++ and more. Enhance your preparation with comprehensive language-specific content.",
+      alternates: {
+         canonical: `https://itsindianguy.in/interview-qa/${slug}`,
+      },
+   };
+}
 const InterviewQAChild = () => {
    return (
       <Fragment>
